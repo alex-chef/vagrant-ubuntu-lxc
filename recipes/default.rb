418ed149c4f9d9618756d9cd12b4102627395ed2
Vincenzo D'Amore <v.damore@gmail.com>
2014-11-18 18:53:22 +0100

    fix vagrant plugin install user

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@ end
 
 execute "apt-get-install-lxc" do
   user "root"
-  command "apt-get -y install lxc wget build-essential"
+  command "apt-get -y install lxc wget git build-essential"
   returns 0
 end
 
@@ -37,26 +37,41 @@ execute "dpkg-install-vagrant" do
 end
 
 execute "vagrant-install-plugin-vagrant-lxc" do
-  user "root"
+  user "vagrant"
   command "vagrant plugin install vagrant-lxc"
   returns 0
 end
 
-execute "vagrant-install-plugin-chef-zero" do
-  user "root"
-  command "vagrant plugin install chef-zero"
-  returns 0
-end
+# execute "vagrant-install-plugin-chef-zero" do
+#   user "vagrant"
+#   command "vagrant plugin install chef-zero"
+#   returns 0
+# end
 
 execute "vagrant-install-plugin-vagrant-omnibus" do
-  user "root"
+  user "vagrant"
   command "vagrant plugin install vagrant-omnibus"
   returns 0
 end
 
 execute "vagrant-install-plugin-vagrant-berkshelf" do
-  user "root"
+  user "vagrant"
   command "vagrant plugin install vagrant-berkshelf"
+  returns 0
+end
+
+execute "wget_chefdk_0.3.5-1_amd64" do
+  user "root"
+  cwd "/tmp"
+  command "wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.5-1_amd64.deb"
+  creates "/tmp/chefdk_0.3.5-1_amd64.deb"
+  returns 0
+end
+
+execute "dpkg-install-chefdk" do
+  user "root"
+  cwd "/tmp"
+  command "dpkg -i chefdk_0.3.5-1_amd64.deb"
   returns 0
 end
 
