e0de0ed528527be4816c22750599d910ffeb87ee
Vincenzo D'Amore <v.damore@gmail.com>
2014-11-19 11:50:50 +0100

    fix vagrant plugin check and install

--- recipes/default.rb
+++ recipes/default.rb
@@ -39,54 +39,54 @@ dpkg_package "vagrant" do
   action :install
 end
 
-# execute "wget_chefdk_0.3.5-1_amd64" do
-#   user "root"
-#   cwd "/tmp"
-#   command "wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.5-1_amd64.deb"
-#   creates "/tmp/chefdk_0.3.5-1_amd64.deb"
-#   returns 0
-# end
-# 
-# dpkg_package "chefdk" do
-#   case node[:platform]
-#   when "debian","ubuntu"
-#     package_name "chefdk"
-#     source "/tmp/chefdk_0.3.5-1_amd64.deb"
-#   end
-#   action :install
-# end
-# 
-# execute "vagrant-install-plugin-vagrant-berkshelf" do
-#   user "vagrant"
-#   cwd "/home/vagrant"
-#   command "vagrant plugin install vagrant-berkshelf"
-#   action :run
-#   environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
-#   returns 0
-# end
-# 
-# execute "vagrant-install-plugin-vagrant-lxc" do
-#   cwd "/home/vagrant"
-#   user "vagrant"
-#   command "vagrant plugin install vagrant-lxc"
-#   action :run
-#   environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
-#   returns 0
-# end
+execute "wget_chefdk_0.3.5-1_amd64" do
+    user "root"
+    cwd "/tmp"
+    command "wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chefdk_0.3.5-1_amd64.deb"
+    creates "/tmp/chefdk_0.3.5-1_amd64.deb"
+    returns 0
+end
 
-# execute "vagrant-install-plugin-chef-zero" do
-#   user "vagrant"
-#   command "vagrant plugin install chef-zero"
-#   environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
-#   returns 0
-# end
+dpkg_package "chefdk" do
+    case node[:platform]
+    when "debian","ubuntu"
+        package_name "chefdk"
+        source "/tmp/chefdk_0.3.5-1_amd64.deb"
+    end
+    action :install
+end
 
-# execute "vagrant-install-plugin-vagrant-omnibus" do
-#   user "vagrant"
-#   cwd "/home/vagrant"
-#   command "vagrant plugin install vagrant-omnibus"
-#   action :run
-#   environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
-#   returns 0
-# end
+#execute "vagrant-install-plugin-vagrant-berkshelf" do
+#    user "vagrant"
+#    cwd "/home/vagrant"
+#    command "vagrant plugin install vagrant-berkshelf"
+#    action :run
+#    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
+#    returns 0
+#end
+
+#execute "vagrant-install-plugin-vagrant-lxc" do
+#    cwd "/home/vagrant"
+#    user "vagrant"
+#    command "vagrant plugin install vagrant-lxc"
+#    action :run
+#    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
+#    returns 0
+#end
+
+#execute "vagrant-install-plugin-chef-zero" do
+#    user "vagrant"
+#    command "vagrant plugin install chef-zero"
+#    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
+#    returns 0
+#end
+
+#execute "vagrant-install-plugin-vagrant-omnibus" do
+#    user "vagrant"
+#    cwd "/home/vagrant"
+#    command "vagrant plugin install vagrant-omnibus"
+#    action :run
+#    environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'}) 
+#    returns 0
+#end
 
